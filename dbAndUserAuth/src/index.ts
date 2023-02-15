65a36a84e4c54a3909dc34bfba06f2f54fc8040a
Joseph Ditton <jditton.atomic@gmail.com>
2023-02-15 12:23:17 -0700

    class

--- dbAndUserAuth/src/index.ts
+++ dbAndUserAuth/src/index.ts
@@ -1,11 +1,13 @@
-import express from "express";
-import { PrismaClient } from "@prisma/client";
+import express, { Request, RequestHandler } from "express";
+import { PrismaClient, Session, User } from "@prisma/client";
 const client = new PrismaClient();
 const app = express();
 import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from 'uuid';
+import cookieParser from "cookie-parser";
 
 app.use(express.json());
+app.use(cookieParser());
 
 type CreateUserBody = {
   firstName: string,
@@ -14,16 +16,57 @@ type CreateUserBody = {
   password: string,
 }
 
+type RequestWithSession = Request & {
+  session?: Session
+  user?: User
+}
+
+const authenticationMiddleware: RequestHandler = async (req: RequestWithSession, res, next) => {
+  const sessionToken = req.cookies["session-token"];
+  if (sessionToken) {
+    const session = await client.session.findFirst({
+      where: {
+        token: sessionToken
+      },
+      include: {
+        user: true
+      }
+    });
+    if (session) {
+      req.session = session;
+      req.user = session.user;
+    }
+  }
+  next();
+}
+
+app.use(authenticationMiddleware);
+
 //sign up
 app.post('/users', async (req, res) => {
   const {firstName, lastName, email, password} = req.body as CreateUserBody;
   const passwordHash = await bcrypt.hash(password, 10);
-  const user = await client.user.create({ data: {
-    firstName,
-    lastName,
-    email,
-    passwordHash,
-  }});
+  const user = await client.user.create({
+    data: {
+      firstName,
+      lastName,
+      email,
+      passwordHash,
+      sessions: {
+        create: [{
+          token: uuidv4()
+        }]
+      }
+    },
+    include: {
+      sessions: true
+    }
+  });
+  res.cookie("session-token", user.sessions[0].token, {
+    httpOnly: true,
+    maxAge: 60000 * 10
+  });
+
   res.json({ user });
 });
 
@@ -32,6 +75,7 @@ type LoginBody = {
   password: string
 }
 
+// log in
 app.post("/sessions",  async (req, res) => {
   const {email, password} = req.body as LoginBody;
   const user = await client.user.findFirst({
@@ -60,10 +104,17 @@ app.post("/sessions",  async (req, res) => {
 
   res.cookie("session-token", session.token, {
     httpOnly: true,
+    maxAge: 60000 * 10
   })
-
 });
 
+app.get("/me", async (req: RequestWithSession, res) => {
+  if (req.session) {
+    res.json({ user: req.user });
+  } else {
+    res.status(401).json({ message: "unauthorized"});
+  }
+})
 
 app.get("/", (req, res) => {
   res.send(`<h1>Hello, world!</h1>`);
---
+++ tokenAuth/prisma/migrations/20230213175827_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
